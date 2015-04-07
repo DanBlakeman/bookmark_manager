ca1da2b6126397bd4f06b8bda4041aaf59c58566
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-07 17:54:54 +0100

    ensure database is cleaned after each test

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ end
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'database_cleaner'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,21 @@
-ENV['RACK_ENV'] = 'test'
+require 'database_cleaner'
 
+ENV['RACK_ENV'] = 'test'
 require_relative '../server'
+
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
