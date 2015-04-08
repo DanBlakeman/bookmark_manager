5aabdde187fa188be46766b3c42dbe665ea22484
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-08 12:26:41 +0100

    links can now have tags

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -7,13 +7,22 @@ feature 'User adds a new link' do
     expect(Link.count).to eq(1)
     link = Link.first
     expect(link.url).to eq('http://www.makersacademy.com/')
-    expect(link.title).to eq('Makers Academy') 
+    expect(link.title).to eq('Makers Academy')
   end
 
-  def add_link(url, title)
+  scenario 'with a few tags' do
+    visit '/'
+    add_link('http://www.makersacademy.com/', 'Makers Academy', %w(education ruby))
+    link = Link.first
+    expect(link.tags.map(&:text)).to include('education')
+     expect(link.tags.map(&:text)).to include('ruby')
+  end
+
+  def add_link(url, title, tags = [])
     within('#new-link') do
       fill_in('url', with: url)
       fill_in('title', with: title)
+      fill_in('tags', with: tags.join(" "))
       click_button('Add link')
     end
   end
