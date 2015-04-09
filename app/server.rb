e138fa7a97e11eefae1df56584061e11ae35efd5
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-04-09 17:57:16 +0300

    Major folder refactoring

--- app/server.rb
+++ app/server.rb
@@ -1,75 +1,22 @@
 require 'sinatra'
 require 'data_mapper'
 require 'rack-flash'
+
+require_relative './controllers/link'
+require_relative './controllers/tag'
+require_relative './controllers/application'
+require_relative './controllers/users'
+require_relative './controllers/sessions'
+
 require_relative './helpers/application'
-require_relative '../lib/link'
-require_relative '../lib/tag'
-require_relative '../lib/user'
+
+require_relative './models/link'
+require_relative './models/tag'
+require_relative './models/user'
+
 require_relative 'data_mapper_setup'
 
 enable :sessions
 use Rack::Flash
 use Rack::MethodOverride
 set :session_secret, 'super secret'
-
-get '/' do
-  @links = Link.all
-  erb :index
-end
-
-post '/links' do
-  url = params[:url]
-  title = params[:title]
-  tags = params[:tags].split(' ').map do |tag|
-    Tag.first_or_create(text: tag)
-  end
-  Link.create(url: url, title: title, tags: tags)
-  redirect to '/'
-end
-
-get '/tags/:text' do
-  tag = Tag.first(text: params[:text])
-  @links = tag ? tag.links : []
-  erb :index
-end
-
-get '/users/new' do
-  @user = User.new
-  erb :'users/new'
-end
-
-post '/users' do
-  @user = User.create(
-    email: params[:email],
-    password: params[:password],
-    password_confirmation: params[:password_confirmation])
-  if @user.save
-    session[:user_id] = @user.id
-    redirect to('/')
-  else
-    flash.now[:errors] = @user.errors.full_messages
-    erb :'users/new'
-  end
-end
-
-get '/sessions/new' do
-  erb :'sessions/new'
-end
-
-post '/sessions' do
-  email, password = params[:email], params[:password]
-  user = User.authenticate(email, password)
-  if user
-    session[:user_id] = user.id
-    redirect '/'
-  else
-    flash[:errors] = ['The email or password is incorrect']
-    erb :'sessions/new'
-  end
-end
-
-delete '/sessions' do
-  session[:user_id] = nil
-  flash[:notice] = 'Good bye!'
-  redirect '/'
-end
