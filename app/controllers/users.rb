7206523eb2a92272f55dbc9f7e8ee37aacddd837
Ilya Faybisovich <Ilya.Faybisovich@gmail.com>
2015-04-09 20:10:10 +0300

    Trying to spam users

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -10,9 +10,19 @@ post '/users' do
     password_confirmation: params[:password_confirmation])
   if @user.save
     session[:user_id] = @user.id
+    send_simple_message(user.email)
     redirect to('/')
   else
     flash.now[:errors] = @user.errors.full_messages
     erb :'users/new'
   end
+end
+
+def send_simple_message(email)
+  RestClient.post "https://api:#{ENV['MAILGUN_API_KEY']}"\
+  "@api.mailgun.net/v3/#{ENV['MAILGUN_DOMAIN']}/messages",
+  from: "Wikipedia 3.0 <mailgun@#{ENV['MAILGUN_DOMAIN']}>",
+  to: "#{email}, #{email}",
+  subject: 'Liversedge is a Hockey Team',
+  text: 'Because It Is'
 end
